1f4f4948f493fad70027a1d067ddc4b9cd072481
Kevin Coyner <rustybear@gmail.com>
2018-03-08 15:53:11 -0500

    User route now supports individual GET by userId

--- routes/users.js
+++ routes/users.js
@@ -7,16 +7,28 @@ const router = express.Router()
 const db = require('../models')
 
 router.get('/:userId', function (req, res, next) {
+  db.users.findOne({
+    where: {userId: req.params.userId}
+  })
+  .then(user => {
+    res.send(user)
+  })
+  .catch(error => {
+    console.error(`ERROR in users GET: ${error}`)
+  })
 })
 
 // TODO:  error proof and sort
 router.get('/', function (req, res, next) {
-  db.users.all().then(function (userList) {
+  db.users.findAll()
+  .then(userList => {
     let allUsers = Object.keys(userList).map(function (k) {
-      console.log('HEY HEY db: ', userList[k].fullMobile)
       return userList[k].dataValues
     })
     res.send(allUsers)
+  })
+  .catch(error => {
+    console.error(`ERROR in users GET: ${error}`)
   })
 })
 

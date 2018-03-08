52bb1a1e3ec2eedd6daeffb69f3fc927e509bb12
Kevin Coyner <rustybear@gmail.com>
2018-03-08 15:25:46 -0500

    Begin work on getting apparatus and users for recipients list

--- routes/calls.js
+++ routes/calls.js
@@ -5,40 +5,32 @@
 const express = require('express')
 const router = express.Router()
 const cuid = require('cuid')
-const subDays = require('date-fns/sub_days')
-// const Call = require('../models/call/call-dynamo')
 const db = require('../models')
 const emailTransporter = require('../util/sendEmailSES')
 
-const DEBUG = true // set this to true to suppress sending POST requests to Dynamo
+const DEBUG = true // set this to true to suppress sending POST requests to Postgres
 
-// GET calls listing for last x numberOfDays
-var numberOfDays = 3
-
-// router.get('/', function (req, res, next) {
-//   let startDate = subDays(Date.now(), numberOfDays)
-//   Call.scan().where('createdAt').gte(startDate).exec( (err, data) => {
-//     let allCalls = Object.keys(data.Items).map(function (k) {
-//       return data.Items[k].attrs
-//     })
-//     let sortedCalls = allCalls.slice(0)
-//     sortedCalls.sort((a, b) => b.cfs_no - a.cfs_no)
-//     let filteredCalls = sortedCalls.filter(call => call.test_call === false )
-//     err ?
-//       console.error(`DYNAMO FETCH ERROR: ${err}`)
-//     :
-//       res.send(filteredCalls)
-//   })
-// })
-
-//TODO:  error proof and sort
+// TODO:  error proof and sort
 router.get('/', function (req, res, next) {
-  let startDate = subDays(Date.now(), numberOfDays)
   db.calls.all().then(function (callList) {
     let allCalls = Object.keys(callList).map(function (k) {
       return callList[k].dataValues
     })
     res.send(allCalls)
+  })
+})
+
+router.get('/track/:appar', function (req, res, next) {
+  console.log('req.params: ', req.params.appar)
+  db.trackings.findAll(
+    {
+      where: {
+        apparatusId: req.params.appar
+      }
+    }
+  ).then(function (tracksList) {
+    let allTracks = Object.keys(tracksList).map(k => tracksList[k].dataValues)
+    res.send(allTracks)
   })
 })
 
@@ -77,13 +69,6 @@ const processData = (data) => {
   return callDetails
 }
 
-const buildRecipientList = (processedData) => {
-  let assignment = processedData.assignment
-  //  assignment is an array
-  // a fancy join between apparatus assigned and the users tracking those
-  // apparatus
-}
-
 const sendToPostgres = (processedData) => {
   db.calls.create(processedData)
   .then(processedData => {
@@ -96,35 +81,24 @@ const sendToPostgres = (processedData) => {
   })
 }
 
-const sendToDynamo = (processedData) => {
-  var newCall = new Call(processedData)
-  newCall.save(function (err) {
-    if (err) {
-      console.log('err: ', err)
-    } else {
-      console.log('created new item in DynamoDB calls: ', newCall.get('call_id'))
-    }
-  })
-}
-
 const sendEmail = (data) => {
   emailTransporter.sendMail({
-      from: 'postmaster@signalclick.com',
-      to: '2035160005@msg.fi.google.com, 8057060651@vtext.com',
-      subject: 'GFD Call',
-      text: `Call type: ${data.call_category}
+    from: 'postmaster@signalclick.com',
+    to: '2035160005@msg.fi.google.com, 8057060651@vtext.com',
+    subject: 'GFD Call',
+    text: `Call type: ${data.call_category}
 Location: ${data.location}  ${data.city}
 Assignment: ${data.assignment}
 Details: https://ers-dispatch.firebaseapp.com/?id=${data.slug}
       `
   }, (err, info) => {
-      if (err) {
-          console.error(err)
-      } else {
-          console.log(info.envelope);
-          console.log(info.messageId);
-      }
-  });
+    if (err) {
+      console.error(err)
+    } else {
+      console.log(info.envelope)
+      console.log(info.messageId)
+    }
+  })
 }
 
 // POST calls listing
--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,14 @@ const express = require('express')
 const router = express.Router()
 const db = require('../models')
 
-//TODO:  error proof and sort
+router.get('/:userId', function (req, res, next) {
+})
+
+// TODO:  error proof and sort
 router.get('/', function (req, res, next) {
   db.users.all().then(function (userList) {
     let allUsers = Object.keys(userList).map(function (k) {
+      console.log('HEY HEY db: ', userList[k].fullMobile)
       return userList[k].dataValues
     })
     res.send(allUsers)

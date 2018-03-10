4f393f6dc7bdef8944255d2d2935b71019d7d451
Kevin Coyner <rustybear@gmail.com>
2018-03-10 07:36:57 -0500

    Ran prettier-standard

--- src/utils/getCoordinates.js
+++ src/utils/getCoordinates.js
@@ -1,13 +1,14 @@
 import axios from 'axios'
 
-export default function getCoordinates(address, district) {
+export default function getCoordinates (address, district) {
+  let addressFormatted = address.split(' ').join('_')
 
-  let addressFormatted = address.split(' ').join('_');
-
-  axios.get(`https://maps.google.com/maps/api/geocode/json?address=${addressFormatted}+${district}+CT&key=AIzaSyDaIBXGdwp9ItpY-lA_rLk7cJ35jorY18k`)
-  .then(response => {
-    console.log(response.data.results[0].geometry.location)
-    return response.data.results[0].geometry.location
-  })
-
+  axios
+    .get(
+      `https://maps.google.com/maps/api/geocode/json?address=${addressFormatted}+${district}+CT&key=AIzaSyDaIBXGdwp9ItpY-lA_rLk7cJ35jorY18k`
+    )
+    .then(response => {
+      console.log(response.data.results[0].geometry.location)
+      return response.data.results[0].geometry.location
+    })
 }
